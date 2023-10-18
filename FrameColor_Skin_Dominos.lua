fdcdbfb774c31117595b4e8958168a752285c659
Slothpala <slothpala@gmail.com>
2023-10-18 16:12:24 +0200

    Initial Commit

---
+++ FrameColor_Skin_Dominos.lua
@@ -0,0 +1,43 @@
+local info = {
+    moduleName = "Dominos",
+    color1 = {
+        name = "Color",
+        desc = "",
+    },
+}
+
+local module = FrameColor_CreateSkinModule(info)
+
+local _G = _G
+local SetDesaturation = SetDesaturation
+local SetVertexColor = SetVertexColor
+
+function module:OnEnable()
+    local main_color = self:GetColor1()
+    self:Recolor(main_color, 1)
+end
+
+function module:OnDisable()
+    local color = {r=1,g=1,b=1,a=1}
+    self:Recolor(color, 0)
+end
+
+function module:Recolor(main_color, desaturation)
+    --actionbars 1 to 14
+    for i=1,168 do
+        local DominosActionButton = _G["DominosActionButton"..i.."NormalTexture"] 
+        if DominosActionButton then
+            DominosActionButton:SetDesaturation(desaturation)
+            DominosActionButton:SetVertexColor(main_color.r,main_color.g,main_color.b,main_color.a)
+        end
+    end
+    --shapeshift bar
+    for i=1, GetNumShapeshiftForms() do
+        local DominosStanceButton = _G["DominosStanceButton"..i.."NormalTexture"] 
+        if DominosStanceButton then
+            DominosStanceButton:SetDesaturation(desaturation)
+            DominosStanceButton:SetVertexColor(main_color.r,main_color.g,main_color.b,main_color.a)
+        end
+    end
+end
+
